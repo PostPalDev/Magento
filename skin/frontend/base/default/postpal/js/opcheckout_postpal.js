3ce6896d2994d84a4964d5ebbe3d2176f09b47bd
Klemens Arro <klemens@adm.ee>
2016-02-25 00:32:42 +0200

    Added support for custom cart templates

--- skin/frontend/base/default/postpal/js/opcheckout_postpal.js
+++ skin/frontend/base/default/postpal/js/opcheckout_postpal.js
@@ -1,42 +1,45 @@
-Review.prototype.nextStep = function(transport){
-    if (transport && transport.responseText) {
-        try{
-            response = eval('(' + transport.responseText + ')');
-        }
-        catch (e) {
-            response = {};
-        }
-        if (response.redirect) {
-            this.isSuccess = true;
-            location.href = response.redirect;
-            return;
-        }
-        if (response.success) {
-            this.isSuccess = true;
-            window.location=this.successUrl;
-        }
-        else{
-            var msg = response.error_messages;
-            if (typeof(msg)=='object') {
-                msg = msg.join("\n");
-            }
-            if (msg) {
-                msg = msg.replace(new RegExp((';').replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"), 'g'), '<br />');
-                if ($('checkout-step-review') != null) {
-                    $('checkout-step-review').update('<div class="error postpal_error_wrapper" id="checkoutError">'+msg+'<span class="postpal_error_close">x</span></div>'+$('checkout-step-review').innerHTML);
-                }
-                else {
-                    alert(msg);
-                }
-            }
-        }
+if(typeof Review != 'undefined') {
 
-        if (response.update_section) {
-            $('checkout-'+response.update_section.name+'-load').update(response.update_section.html);
-        }
+    Review.prototype.nextStep = function (transport) {
+        if (transport && transport.responseText) {
+            try {
+                response = eval('(' + transport.responseText + ')');
+            }
+            catch (e) {
+                response = {};
+            }
+            if (response.redirect) {
+                this.isSuccess = true;
+                location.href = response.redirect;
+                return;
+            }
+            if (response.success) {
+                this.isSuccess = true;
+                window.location = this.successUrl;
+            }
+            else {
+                var msg = response.error_messages;
+                if (typeof(msg) == 'object') {
+                    msg = msg.join("\n");
+                }
+                if (msg) {
+                    msg = msg.replace(new RegExp((';').replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"), 'g'), '<br />');
+                    if ($('checkout-step-review') != null) {
+                        $('checkout-step-review').update('<div class="error postpal_error_wrapper" id="checkoutError">' + msg + '<span class="postpal_error_close">x</span></div>' + $('checkout-step-review').innerHTML);
+                    }
+                    else {
+                        alert(msg);
+                    }
+                }
+            }
 
-        if (response.goto_section) {
-            checkout.gotoSection(response.goto_section, true);
+            if (response.update_section) {
+                $('checkout-' + response.update_section.name + '-load').update(response.update_section.html);
+            }
+
+            if (response.goto_section) {
+                checkout.gotoSection(response.goto_section, true);
+            }
         }
     }
 }
